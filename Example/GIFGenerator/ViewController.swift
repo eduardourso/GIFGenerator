4e47f788ca03e052d2f39ecce3d7854e078c7d25
Eduardo Urso <eduardourso@gmail.com>
2016-01-26 12:41:45 +0100

    Added example of how to use the library

--- Example/GIFGenerator/ViewController.swift
+++ Example/GIFGenerator/ViewController.swift
@@ -7,18 +7,36 @@
 //
 
 import UIKit
+import GIFGenerator
 
 class ViewController: UIViewController {
+    
+    let gifGenerator = GifGenerator()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //        let images:[UIImage] = [UIImage(named: "homer-simpson.jpg")!, UIImage(named: "homer-simpson2.jpg")!]
+        let images2:[UIImage] = [UIImage(named: "bart1.jpg")!, UIImage(named: "bart2.jpg")!, UIImage(named: "bart3.jpg")!, UIImage(named: "bart4.jpg")!, UIImage(named: "bart5.jpg")!]
+        
+        let documentsPath : String = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask,true)[0]
+        if let destinationPath :String = documentsPath.stringByAppendingString("/animated.gif") {
+            
+            gifGenerator.generateGifFromImages(images2, frameDelay: 0.5, destinationURL: NSURL(fileURLWithPath: destinationPath), callback: { (data, error) -> () in
+                if error == nil {
+                    if let data = data {
+                    let image = UIImage(data: data)
+                    let imageView = UIImageView(image: image)
+                    imageView.image = image
+                    imageView.frame = CGRectMake(0, 0, image!.size.width/2, image!.size.height/2)
+                    imageView.center = self.view.center
+                    self.view.addSubview(imageView)
+                    }
+                }
+                
+            })
+    
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
