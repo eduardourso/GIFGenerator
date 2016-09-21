ed18d5d0be35f14c03ad33bf4fca252988a7e74e
Eduardo Urso <eduardourso@gmail.com>
2016-09-21 15:15:45 +0200

    fix for supporting Xcode 8 and swift 3

--- Pod/Classes/GIFGenerator.swift
+++ Pod/Classes/GIFGenerator.swift
@@ -3,7 +3,7 @@ import MobileCoreServices
 import AVFoundation
 
 
-@objc public class GifGenerator: NSObject {
+@objc open class GifGenerator: NSObject {
     
     var cmTimeArray:[NSValue] = []
     var framesArray:[UIImage] = []
@@ -17,17 +17,17 @@ import AVFoundation
      :param: frameDelay an delay in seconds between each frame
      :param: callback set a block that will get called when done, it'll return with data and error, both which can be nil
      */
-    public func generateGifFromImages(imagesArray imagesArray:[UIImage], repeatCount: Int = 0, frameDelay: NSTimeInterval, destinationURL: NSURL, callback:(data: NSData?, error: NSError?) -> ()) {
+    open func generateGifFromImages(imagesArray:[UIImage], repeatCount: Int = 0, frameDelay: TimeInterval, destinationURL: URL, callback:@escaping (_ data: Data?, _ error: NSError?) -> ()) {
         
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) { () -> Void in
+        DispatchQueue.global(priority: DispatchQueue.GlobalQueuePriority.background).async { () -> Void in
             
-            if let imageDestination = CGImageDestinationCreateWithURL(destinationURL, kUTTypeGIF, imagesArray.count, nil) {
+            if let imageDestination = CGImageDestinationCreateWithURL(destinationURL as CFURL, kUTTypeGIF, imagesArray.count, nil) {
                 
-                let frameProperties:CFDictionaryRef = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFDelayTime as String: frameDelay]]
-                let gifProperties:CFDictionaryRef = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFLoopCount as String: repeatCount]]
+                let frameProperties:CFDictionary = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFDelayTime as String: frameDelay]]
+                let gifProperties:CFDictionary = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFLoopCount as String: repeatCount]]
                 
                 for image in imagesArray {
-                    CGImageDestinationAddImage(imageDestination, image.CGImage!, frameProperties)
+                    CGImageDestinationAddImage(imageDestination, image.cgImage!, frameProperties)
                 }
                 
                 CGImageDestinationSetProperties(imageDestination, gifProperties)
@@ -37,19 +37,19 @@ import AVFoundation
                     
                     do {
                         if let path = destinationURL.path {
-                            let attr : NSDictionary? = try NSFileManager.defaultManager().attributesOfItemAtPath(path)
+                            let attr : NSDictionary? = try FileManager.default.attributesOfItem(atPath: path)
                             
                             if let _attr = attr {
-                                print("FILE SIZE: ", NSByteCountFormatter.stringFromByteCount(Int64(_attr.fileSize()), countStyle: .File))
+                                print("FILE SIZE: ", ByteCountFormatter.string(fromByteCount: Int64(_attr.fileSize()), countStyle: .file))
                             }
                         }
                     } catch {
                         print("Error: \(error)")
                     }
                     
-                    callback(data: NSData(contentsOfURL: destinationURL), error: nil)
+                    callback(try? Data(contentsOf: destinationURL), nil)
                 } else {
-                    callback(data: nil, error: self.errorFromString("Couldn't create the final image"))
+                    callback(nil, self.errorFromString("Couldn't create the final image"))
                 }
             }
         }
@@ -64,36 +64,36 @@ import AVFoundation
      :param: frameDelay an delay in seconds between each frame
      :param: callback set a block that will get called when done, it'll return with data and error, both which can be nil
      */
-    public func generateGifFromVideoURL(videoURL videoUrl:NSURL, repeatCount: Int = 0, framesInterval:Int, frameDelay: NSTimeInterval, destinationURL: NSURL, callback:(data: NSData?, error: NSError?) -> ()) {
+    open func generateGifFromVideoURL(videoURL videoUrl:URL, repeatCount: Int = 0, framesInterval:Int, frameDelay: TimeInterval, destinationURL: URL, callback:@escaping (_ data: Data?, _ error: NSError?) -> ()) {
         
         self.generateFrames(videoUrl, framesInterval: framesInterval) { (images) -> () in
             if let images = images {
                 self.generateGifFromImages(imagesArray: images, repeatCount: repeatCount, frameDelay: frameDelay, destinationURL: destinationURL, callback: { (data, error) -> () in
                     self.cmTimeArray = []
                     self.framesArray = []
-                    callback(data: data, error: error)
+                    callback(data, error)
                 })
             }
         }
     }
     
     // MARK: THANKS TO: http://stackoverflow.com/questions/4001755/trying-to-understand-cmtime-and-cmtimemake
-   private func generateFrames(url:NSURL, framesInterval:Int, callback:(images:[UIImage]?) -> ()) {
+   fileprivate func generateFrames(_ url:URL, framesInterval:Int, callback:@escaping (_ images:[UIImage]?) -> ()) {
         
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) { () -> Void in
-            self.generateCMTimesArrayOfFramesUsingAsset(framesInterval, asset: AVURLAsset(URL: url))
+        DispatchQueue.global(priority: DispatchQueue.GlobalQueuePriority.background).async { () -> Void in
+            self.generateCMTimesArrayOfFramesUsingAsset(framesInterval, asset: AVURLAsset(url: url))
             
             var i = 0
             let test:AVAssetImageGeneratorCompletionHandler = { (_, im:CGImage?, _, result:AVAssetImageGeneratorResult, error:NSError?) in
-                if(result == AVAssetImageGeneratorResult.Succeeded) {
+                if(result == AVAssetImageGeneratorResult.succeeded) {
                     print("Succeed")
                     if let image = im {
-                        self.framesArray.append(UIImage(CGImage: image))
+                        self.framesArray.append(UIImage(cgImage: image))
                     }
-                } else if (result == AVAssetImageGeneratorResult.Failed) {
+                } else if (result == AVAssetImageGeneratorResult.failed) {
                     print("Failed with error")
                     callback(images: nil);
-                } else if (result == AVAssetImageGeneratorResult.Cancelled) {
+                } else if (result == AVAssetImageGeneratorResult.cancelled) {
                     print("Canceled")
                     callback(images: nil);
                 }
@@ -103,30 +103,30 @@ import AVFoundation
                     callback(images: self.framesArray)
                 }
             }
-            let generator = AVAssetImageGenerator(asset: AVAsset(URL: url))
+            let generator = AVAssetImageGenerator(asset: AVAsset(url: url))
             generator.apertureMode = AVAssetImageGeneratorApertureModeCleanAperture;
             generator.appliesPreferredTrackTransform = true;
             generator.requestedTimeToleranceBefore = kCMTimeZero;
             generator.requestedTimeToleranceAfter = kCMTimeZero;
-            generator.maximumSize = CGSizeMake(200, 200);
+//            generator.maximumSize = CGSize(width: 200, height: 200);
             
-            generator.generateCGImagesAsynchronouslyForTimes(self.cmTimeArray, completionHandler: test)
+            generator.generateCGImagesAsynchronously(forTimes: self.cmTimeArray, completionHandler: test)
         }
     }
     
-   private func generateCMTimesArrayOfAllFramesUsingAsset(asset:AVURLAsset) {
+   fileprivate func generateCMTimesArrayOfAllFramesUsingAsset(_ asset:AVURLAsset) {
         if cmTimeArray.count > 0 {
             cmTimeArray.removeAll()
         }
         
-        for (var t:Int64 = 0; t < asset.duration.value; t++) {
+        for (t:Int64 in 0 ..< asset.duration.value) {
             let thumbTime = CMTimeMake(t, asset.duration.timescale)
-            let value = NSValue(CMTime: thumbTime)
+            let value = NSValue(time: thumbTime)
             cmTimeArray.append(value)
         }
     }
     
-    private func generateCMTimesArrayOfFramesUsingAsset(framesInterval:Int, asset:AVURLAsset) {
+    fileprivate func generateCMTimesArrayOfFramesUsingAsset(_ framesInterval:Int, asset:AVURLAsset) {
         
         let videoDuration = Int(ceilf((Float(Int(asset.duration.value)/Int(asset.duration.timescale)))))
         
@@ -134,20 +134,20 @@ import AVFoundation
             cmTimeArray.removeAll()
         }
         
-        for (var t = 0; t < videoDuration; t++) {
+        for (t in 0 ..< videoDuration) {
             let tempInt = Int64(t)
             let tempCMTime = CMTimeMake(tempInt, asset.duration.timescale)
             let interval = Int32(framesInterval)
             
-            for (var j = 1; j < framesInterval+1; j++) {
+            for (j in 1 ..< framesInterval+1) {
                 let newCMtime = CMTimeMake(Int64(j), interval)
                 let addition = CMTimeAdd(tempCMTime, newCMtime)
-                cmTimeArray.append(NSValue(CMTime: addition))
+                cmTimeArray.append(NSValue(time: addition))
             }
         }
     }
 
-    private func errorFromString(string: String, code: Int = -1) -> NSError {
+    fileprivate func errorFromString(_ string: String, code: Int = -1) -> NSError {
         let dict = [NSLocalizedDescriptionKey: string]
         return NSError(domain: "org.cocoapods.GIFGenerator", code: code, userInfo: dict)
     }
